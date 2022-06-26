91b47188aee9c536d3362931f3a1b690db2eb75d
donrestarone <shashikejayatunge@gmail.com>
2022-06-26 13:53:44 -0400

    init

--- Violet Rails/ViewController.swift
+++ Violet Rails/ViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UINavigationController, UITabBarDelegate {
+    let tabBar = UITabBar()
+    let home = UITabBarItem(title: "Home",image: .checkmark, tag: 0)
+    let blog = UITabBarItem(title: "Blog", image: .checkmark, tag: 1)
+    let forum = UITabBarItem(title: "Forum", image: .checkmark, tag: 2)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tabBar.frame = CGRect(x: 0, y: self.view.frame.height - 80, width: self.view.frame.width, height: 49)
+        tabBar.items = [home, blog, forum]
+        self.view.addSubview(tabBar)
     }
 
 
