718a3ef047253624ef683e01fe482bfea9f7a02a
Don Restarone <35935196+donrestarone@users.noreply.github.com>
2023-04-28 21:04:37 -0400

    App Store review changes
    * bump version and build
    
    * bump version again;;
    c
    
    * Perfomance fixes (#18)
    
    * created a seperate session per tab
    
    * created custom visitable and tab bar controller
    
    ---------
    
    Co-authored-by: josessolmaximo <112497883+josessolmaximo@users.noreply.github.com>

---
+++ Violet Rails/BaseVisitableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  BaseVisitableViewController.swift
+//  Violet Rails
+//
+//  Created by Joses Solmaximo on 23/04/23.
+//
+
+import UIKit
+import WebKit
+import Turbo
+
+class BaseVisitableViewController: UIViewController, Visitable {
+    weak var visitableDelegate: VisitableDelegate?
+    var visitableURL: URL!
+    
+    private lazy var session: Session = {
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "VioletRailsiOS"
+        let session = Session(webViewConfiguration: configuration)
+        session.delegate = self
+        return session
+    }()
+
+    public convenience init(url: URL) {
+        self.init()
+        self.visitableURL = url
+    }
+    
+    // MARK: View Lifecycle
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        installVisitableView()
+    }
+
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        visitableDelegate?.visitableViewWillAppear(self)
+    }
+
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        visitableDelegate?.visitableViewDidAppear(self)
+    }
+
+    // MARK: Visitable
+
+    open func visitableDidRender() {
+        navigationItem.title = visitableView.webView?.title
+    }
+    
+    open func showVisitableActivityIndicator() {
+        visitableView.showActivityIndicator()
+    }
+    
+    open func hideVisitableActivityIndicator() {
+        visitableView.hideActivityIndicator()
+    }
+    
+    // MARK: Visitable View
+
+    open private(set) lazy var visitableView: VisitableView! = {
+        let view = VisitableView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private func installVisitableView() {
+        view.addSubview(visitableView)
+        
+        NSLayoutConstraint.activate([
+            visitableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            visitableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            visitableView.topAnchor.constraint(equalTo: view.topAnchor),
+            visitableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        visit(url: visitableURL)
+    }
+    
+    private func visit(url: URL) {
+        visitableURL = url
+        session.visit(self)
+    }
+}
+
+extension BaseVisitableViewController: SessionDelegate {
+    func session(_ session: Turbo.Session, didFailRequestForVisitable visitable: Turbo.Visitable, error: Error) {
+        
+    }
+    
+    func session(_ session: Turbo.Session, didProposeVisit proposal: Turbo.VisitProposal) {
+        visit(url: proposal.url)
+    }
+    
+    func sessionWebViewProcessDidTerminate(_ session: Turbo.Session) {
+        
+    }
+}
